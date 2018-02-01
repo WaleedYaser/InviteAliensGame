5d59e4abeb1d4332ea41b155bbd7c68ee3a10c83
Khalid Hassan <khalidhsoliman@gmail.com>
2018-02-01 20:33:57 +0200

    AddCharactersPackage

---
+++ InviteAliens/Assets/Scripts/LightFlashingBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlashingBehaviour : MonoBehaviour {
+
+	public float speed;
+	public float scale;
+	private float initIntensity;
+	private Light lightComponent;
+	void Start () {
+		lightComponent = GetComponent<Light>();
+		initIntensity = lightComponent.intensity;
+	}
+	
+	void Update () {
+		lightComponent.intensity = initIntensity + Mathf.Sin(Time.time * speed * Mathf.Deg2Rad) * scale;
+	}
+}
